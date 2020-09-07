cfb41de8a6df4b2c59a2eccc5302fb0ce30cd3e3
PiterGroot <70685433+PiterGroot@users.noreply.github.com>
2020-09-07 15:17:04 +0200

    Add files via upload

---
+++ Hello you python.py
@@ -0,0 +1,25 @@
+number = 17
+print("Hello You!, ik ben Piter")
+print("Wie ben jij?")
+x = input()
+print("Hello " + x )
+print(x +  ", hoe oud ben jij?")
+xe = input()
+print("Ik ben 17 jaar oud" )
+
+while True:
+    if (int(xe) < number ):
+        print("Ik ben dan ouder")
+
+    elif (int(xe) == number ):
+        print("Wij zijn even oud")
+
+    elif (int(xe) > number ):
+        print("Dan ben jij ouder")
+
+          
+        
+    
+
+
+
