76839112cdcb7455996f25ae9e9259ec9e210580
PiterGroot <70685433+PiterGroot@users.noreply.github.com>
2020-09-08 10:27:26 +0200

    Update Hello you python.py

--- Hello you python.py
+++ Hello you python.py
@@ -1,24 +1,29 @@
 import sys
 
 number = 17
-print("Hello You!, ik ben Piter")
+
+print("Hello You!, ik ben Piter.")
+
 print("Wie ben jij?")
 x = input()
+
 print("Hello " + x )
 print(x +  ", hoe oud ben jij?")
-print("Ik ben 17 jaar oud" )
+
+print("Ik ben 17 jaar oud." )
 xe = input()
 
 
 while True:
     if (int(xe) < number ):
-        print("Ik ben dan ouder")
+        print(xe + ", ik ben dan ouder.")
 
     elif (int(xe) == number ):
-        print("Wij zijn dan even oud")
+        print(xe + ", wij zijn dan even oud.")
 
     elif (int(xe) > number ):
-        print("Dan ben jij ouder")
+        print(xe + ", dan ben jij ouder.")
+    
     wait = input()
     sys.exit()
           
