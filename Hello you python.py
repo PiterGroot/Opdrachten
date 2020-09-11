6f2714782a9464b1161a56802992f62dc0acf302
PiterGroot <70685433+PiterGroot@users.noreply.github.com>
2020-09-11 15:26:42 +0200

    Extra features Hello You python

--- Hello you python.py
+++ Hello you python.py
@@ -1,31 +1,64 @@
 import sys
-
-number = 17
-
-print("Hello You!, ik ben Piter.")
-
-print("Wie ben jij?")
-x = input()
-
-print("Hello " + x )
-print(x +  ", hoe oud ben jij?")
-
-print("Ik ben 17 jaar oud." )
-xe = input()
+import datetime
+import os
 
 
-while True:
+def Start():
+    x = ""
+    date_time = datetime.datetime.now()
+
+    print("Hello You!, ik ben Piter.")
+
+    print("Wie ben jij?")
+    x = input()
+
+    print("Hello " + x + "\n")
+    print("Het is nu " + str(date_time) + "\n")
+
+    print(x +  ", hoe oud ben jij?" + "\n")
+
+
+    
+
+Start()
+
+def checkAge():
+    number = 17
+    xe = ""
+    print("Ik ben namelijk 17 jaar oud." )
+    xe = input()
+    
     if (int(xe) < number ):
-        print(xe + ", ik ben dan ouder.")
+        print(xe + ", ik ben dan ouder." + "\n")
+        
 
     elif (int(xe) == number ):
-        print(xe + ", wij zijn dan even oud.")
+        print(xe + ", wij zijn dan even oud." + "\n")
+        
 
     elif (int(xe) > number ):
-        print(xe + ", dan ben jij ouder.")
-    
-    wait = input()
-    sys.exit()
+        print(xe + ", dan ben jij ouder." + "\n")
+checkAge()
+
+
+def checkAwnser():
+    print("Wil je dit programma nog een keer opnieuw uitvoeren (y / n)")
+    yes_no = input()
+    if (yes_no == "y"):
+        print("hoi")
+        os.execl(sys.executable, '"{}"'.format(sys.executable), *sys.argv)  
+        
+    if (yes_no == "n"):      
+        print("Doei")
+        sys.exit()
+    else:
+        print('Invalid input, voer alstublieft "y / n" in.' + "\n")
+        checkAwnser()
+
+checkAwnser()
+
+#gemaakt door Piter Groot SD1DA
+
           
         
     
